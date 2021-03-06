06d535035867a4f2497ae65a004725d4191b1e65
bury <306776398@qq.com>
2021-03-07 01:37:42 +0800

    day03

---
+++ code1/day03/01-pointers.go
@@ -0,0 +1,29 @@
+package main
+
+import(
+    "fmt"
+)
+
+
+func main(){
+    i, j := 10, 20
+
+    p := &i
+    fmt.Println(p)
+
+    *p = 100
+    fmt.Println(i)
+
+    m := &p
+    fmt.Println(m)
+  
+    **m = 520
+
+    fmt.Printf("m: %p,P: %p,I: %d\n", m, p, i)
+
+    p = &j
+    fmt.Println(p)
+
+    *p = 200
+    fmt.Println(j)    
+}
