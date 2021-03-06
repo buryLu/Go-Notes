868887646faecbc19bd89810504345c756df2b01
bury <306776398@qq.com>
2021-03-06 15:27:41 +0800

    day01

---
+++ code1/day01/09-zero.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+    "fmt"
+
+)
+
+func main(){
+    var i int
+    var f float64
+    var a bool
+    var b string
+
+    fmt.Printf("%v %v %v %v %q\n", i, f, a, b)
+
+}
---
+++ code1/day01/11-constants.go
@@ -0,0 +1,17 @@
+package main 
+
+import(
+    "fmt"
+
+)
+
+const Pi = 3.14
+
+
+func main(){
+    const world = "世界"
+    fmt.Println("Hello ", world)
+
+    const truth = true
+    fmt.Println("Go rules? ", truth)
+}
