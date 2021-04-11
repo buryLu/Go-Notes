9f079688f274c5634ac1ded9599a9edf26735083
buryLu <306776398@qq.com>
2021-04-11 22:36:43 +0800

    update

--- 01_Go核心编程/01-基础语法/01-hello.go
+++ 01_Go核心编程/01-基础语法/01-hello.go
@@ -3,6 +3,10 @@
 2.go程序都是以一个包开头，同一个包下面属于同一个工程文件可以直接使用，不用import导入
 3.import关键字表示我们所需要导入的包，导入的包必须使用，否则编译器会报错
 4.main()为程序的入口
+5.go程序运行有两种方法：
+	(1)go run codename.go
+	(2)go build命令:在任意路径下执行 go install codename,Go编译器会去GOROOT或GOPATH下的src目录寻找源码文件，
+	在程序包中找main包的main函数作为程序入口然后编译。然后会在GOPAT目录下创建一个bin目录，并把编译好的二进制文件存放进去
 */
 
 package main
@@ -18,3 +22,15 @@ func main() {
 	fmt.Println("Hello Golang and I'm coming")
 
 }
+
+/*
+go run背后的原理也是生成一份临时的go二进制文件然后执行。
+GOROOT=/usr/local/go #gosetup
+GOPATH=/Users/burylu/code/Go-Notes:/Users/burylu/goProject #gosetup
+/usr/local/go/bin/go build -o /private/var/folders/lw/p__200g50rx2rfj33qcwwzvr0000gn/T/___go_build_01_hello_go__2_ /Users/burylu/code/Go-Notes/01_Go核心编程/01-基础语法/01-hello.go #gosetup
+/private/var/folders/lw/p__200g50rx2rfj33qcwwzvr0000gn/T/___go_build_01_hello_go__2_
+
+输出结果：
+Hello Golang and I'm coming
+
+*/
